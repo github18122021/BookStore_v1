586df7f62ac46589fb603b55c3233f0ae6a00b70
Vraj Bagadiya <96316547+github18122021@users.noreply.github.com>
2024-04-16 14:46:07 -0400

    Delete Shopping_cart_backend directory

--- Shopping_cart_backend/routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let router = express.Router();
-
-let bookController = require("../controllers/bookControllers");
-
-router.get("/books", bookController.getBooks);
-
-
-module.exports = router;
